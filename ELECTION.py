efb7c164cd1e80032471a8ad59e0748213078c1e
bartek <barcar09@gmail.com>
2018-03-03 14:19:25 +0100

    Add some if / elif

--- ELECTION.py
+++ ELECTION.py
@@ -1,18 +1,25 @@
 import numpy
 import matplotlib.pyplot as plt
 import collections
-def ELECTION(S):
+import math
+def ELECTION(num_of_trys, S, deterministic_or_not):
     iterator=0
     slot=""
     while(slot!="SINGLE"):
         iterator+=1
-        n=S
+       
         pi_vector=[]
-        values_iterator=0
-        
-        for i in range(n):
-            z=numpy.random.choice([0,1],p=[1-1/n,1/n])  #przypadek 
-            pi_vector.append(z)  #
+        values_iterator = 0
+        if deterministic_or_not=="DETERMINISTIC":
+            n = S
+            for el in range(n):
+                z = numpy.random.choice([0,1],p=[1-1/n,1/n])  #przypadek z wiadomą liczbą urządzeń = 100 
+                pi_vector.append(z)
+        elif deterministic_or_not == "NOT DETERMINISTIC":
+            n = math.ceil(math.log(S,2))
+            for el in range(n):
+                z = numpy.random.choice([0,1],p=[1-1/2**n,1/2**n])
+                pi_vector.append(z)
                        
 
         for el in pi_vector:
@@ -26,12 +33,14 @@ def ELECTION(S):
         else:
             slot="NULL"
     return(iterator)
-def returnning_function(num_of_trys):
+def returnning_function(num_of_trys,S,deterministic_or_not):
     values_i=[]
     dict_counter_i=collections.OrderedDict({})
     for el in range(num_of_trys):
-        f=ELECTION(100)
-        values_i.append(f)
+        if deterministic_or_not=="DETERMINISTIC":
+            f=ELECTION(S)
+        elif deterministic_or_not == "NOT DETERMINISTIC":
+        values_i.append(num_of_devices)
         if str(f) not in dict_counter_i.keys():
             dict_counter_i[str(f)]=1
         else:
